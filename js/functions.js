66a84a2d61fe5da4f0482031867d163f3d22c410
RominaMalimban <romina.lyn@hotmail.it>
2022-11-17 15:35:51 +0100

    funzione per creare numeri random

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,10 @@ function newElement(){
     element.classList.add("box");
     return element;
 }
+
+// Funzione per creare numeri randomici con min e max:
+function numRandom (min, max){
+    return (Math.floor(Math.random() * ((max + 1) - min) + min));
+}
+
+// Funzione per creare array di 16 numeri randomici: 
