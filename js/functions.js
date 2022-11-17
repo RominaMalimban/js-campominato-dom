67a285c8bc3aaa659375d23d7237b32af8b05109
RominaMalimban <romina.lyn@hotmail.it>
2022-11-17 15:28:32 +0100

    copiato e incollato esercizio precedente

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+// FUNCTIONS:
+
+// Funzione per creare la cella:
+function newElement(){
+    let element = document.createElement("div");
+
+    element.classList.add("box");
+    return element;
+}
---
+++ js/script.js
@@ -0,0 +1,34 @@
+// variabili manipolazione DOM:
+let button = document.querySelector("button");
+let main = document.querySelector("main");
+let container = document.querySelector(".container");
+
+// al click su bottone mi genero la griglia:
+button.addEventListener("click",
+    function(){
+        
+        // 1- prendo il div container e lo svuoto ogni volta al click:
+        container.innerHTML = "";
+
+        // 2- mi servo del ciclo per generarmi 64 boxes tramite la funzione:
+        for (let i = 1; i <= 100; i++){
+
+            let cella = newElement();
+
+            // aggancio la variabile cella al suo contenitore e ci inserisco il numero:
+            container.append(cella);
+            cella.innerHTML = i;
+
+            // devo creare una funzione in cui al click sulla cella, questa si colora:
+            cella.addEventListener("click",
+                function(){
+                    cella.classList.add("clicked-box");
+                    console.log(i);
+                }
+            );
+        }
+
+    }
+);
+
+
